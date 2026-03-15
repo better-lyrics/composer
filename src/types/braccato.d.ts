88f6b79221e5ce80fca9283731a7a0c4e5f56fc8
Boidushya <boidushyabhattacharya@gmail.com>
2026-03-15 19:22:11 +0530

    fix: types

--- src/types/braccato.d.ts
+++ src/types/braccato.d.ts
@@ -1,15 +1,19 @@
-declare namespace JSX {
-  interface IntrinsicElements {
-    "braccato-lyrics": React.DetailedHTMLProps<
-      React.HTMLAttributes<HTMLElement> & {
-        source?: string;
-        src?: string;
-        playing?: boolean;
-        "current-time"?: number;
-        "scroll-mode"?: "internal" | "external";
-        dir?: "auto" | "ltr" | "rtl";
-      },
-      HTMLElement
-    >;
+import "react";
+
+declare module "react" {
+  namespace JSX {
+    interface IntrinsicElements {
+      "braccato-lyrics": React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLElement> & {
+          source?: string;
+          src?: string;
+          playing?: boolean;
+          "current-time"?: number;
+          "scroll-mode"?: "internal" | "external";
+          dir?: "auto" | "ltr" | "rtl";
+        },
+        HTMLElement
+      >;
+    }
   }
 }
