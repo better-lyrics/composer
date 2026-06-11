69f869a96cb64c1eb2263dd65559b299199d49f5
Boidushya <boidushyabhattacharya@gmail.com>
2026-06-12 02:16:29 +0530

    feat: persist rolling and preview defaults

--- src/views/timeline/timeline-store.ts
+++ src/views/timeline/timeline-store.ts
@@ -99,7 +99,7 @@ const useTimelineStore = create<TimelineState & TimelineActions>((set, get) => {
   return {
     zoom: settings.defaultZoom,
     followEnabled: settings.followPlayhead,
-    previewSidebarOpen: false,
+    previewSidebarOpen: settings.defaultPreviewSidebar,
     selectedWords: [],
 
     clipboard: null,
@@ -111,7 +111,7 @@ const useTimelineStore = create<TimelineState & TimelineActions>((set, get) => {
     dragTime: 0,
     contextMenu: null,
     editingWord: null,
-    rollingEditMode: false,
+    rollingEditMode: settings.defaultRollingEdit,
     collapsedInstances: {},
     pingingGroupId: null,
     renamingGroupId: null,
